27b74758434baa101b22831045469133ece7a1f3
Lucy Ruiz <lucyruiz846@gmail.com>
2022-08-17 16:26:09 -0700

    Created story and first dictionary

---
+++ game1.py
@@ -0,0 +1,15 @@
+def story():
+    story_hercules = '''
+    Hercules was a strong and brave man.
+    He lived in Greece. The King was jealous of Hercules.
+    People might make Hercules the King. Therefore he wanted to get rid of Hercules.
+    He set difficult tasks for Hercules to keep him away from the country,
+    so that he would not be a possible threat to him as a king\n'''
+    print(story_hercules)
+
+story()
+
+levels_dict = {
+'health': {'enemy1' : 50, 'enemy2' : 20, 'enemy3' : 100},
+'attack_name_power': {'slap' : 10, 'kick': 15, 'punch' : 20}
+}
